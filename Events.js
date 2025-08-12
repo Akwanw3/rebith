f24f8d8f44eccfd54fd13aae18dd54d9dba285b9
ABIA AKPE <akwanwesunday@gmail.com>
2025-08-12 15:30:12 +0100

    fghjgfgh

--- Events.js
+++ Events.js
@@ -15,4 +15,13 @@ form.addEventListener('submit', function(event){
 let input = document.getElementById('myinput');
 input.addEventListener('keyup', function(){
     alert(`you typed: ${input.value}`)
-})    
+})  
+
+
+let form2 = document.getElementById('greet');
+form2.addEventListener('submit', function(event){
+    event.preventDefault();
+    let name = document.getElementById('greetName');
+    alert(`Welcome, ${name.value}!`);
+});
+  
